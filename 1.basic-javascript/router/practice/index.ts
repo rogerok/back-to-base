71ee93247ed6f69696e58177f32260f0d089aa6c
rogerok <fedorik833@gmail.com>
2026-02-01 12:06:27 +0300

    improved typing

--- 1.basic-javascript/router/practice/index.ts
+++ 1.basic-javascript/router/practice/index.ts
@@ -1,32 +1,39 @@
+type Request = Record<string, any>;
+type Response = Record<string, any>;
+
 type TPath = string;
 
 type Next = () => void;
 
-// type Middleware = (req: Request, res: Response, next: Next) => Promise<void> | void;
 type Middleware = (req: Request, res: Response, next: Next) => void;
 
 type MiddlewareMap = {
   [key: TPath]: Middleware[] | undefined;
 };
 
-interface Router {
-  handle(req: Request, res: Response): void;
+interface IRouter {
+  basePath: TPath;
+  readonly middlewareMap: MiddlewareMap;
 
-  use(path: string, middleware: Middleware): void;
+  handle(path: TPath, req: Request, res: Response): void;
+
+  use(path: TPath, middleware: Middleware): void;
 }
 
 const BasePath = "/";
+
+// TODO: how to implement handling middlewares for all routes?
 const AllPath = "*";
 
-export class Router {
-  middlewareMap: MiddlewareMap = {};
+export class Router implements IRouter {
   basePath: TPath;
+  middlewareMap: MiddlewareMap = {};
 
   constructor(basePath: TPath = BasePath) {
     this.basePath = basePath;
   }
 
-  use = (path: TPath = this.basePath, middleware: Middleware) => {
+  use = (path: TPath = this.basePath, middleware: Middleware): void => {
     if (!this.middlewareMap[path]) {
       this.middlewareMap[path] = [];
     }
@@ -34,16 +41,20 @@ export class Router {
     this.middlewareMap[path].push(middleware);
   };
 
-  handle = (path: string, req: Request, res: Response) => {
+  handle = (path: string, req: Request, res: Response): void => {
     let counter = 0;
     const middlewares = this.middlewareMap[path];
+    const allRoutesMiddlewares = this.middlewareMap[AllPath];
 
-    const next = () => {
+    const next = (): void => {
       counter += 1;
       go();
     };
 
-    const go = () => {
+    const go = (): void => {
+      if (allRoutesMiddlewares && allRoutesMiddlewares.length > counter) {
+        allRoutesMiddlewares[counter]?.(req, res, next);
+      }
       if (middlewares && middlewares.length > counter) {
         middlewares[counter]?.(req, res, next);
       }
@@ -52,23 +63,23 @@ export class Router {
     go();
   };
 
-  get = (path: string) => {
-    // this.handle(path);
+  get = (path: string, req: Request, res: Response): void => {
+    this.handle(path, req, res);
   };
 }
 
 const router = new Router();
 
-router.use("/", (req: Request, res: Response, next: Next) => {
+router.use("*", (_: Request, __: Response, next: Next) => {
   console.log("start first");
   next();
   console.log("end first");
 });
 
-router.use("/", (req: Request, res: Response, next: Next) => {
+router.use("/", (_: Request, __: Response, next: Next) => {
   console.log("start second");
   next();
   console.log("end second");
 });
 
-router.get("/");
+router.get("/", {}, {});
