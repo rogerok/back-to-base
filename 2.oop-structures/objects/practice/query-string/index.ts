3a4102e56d557c9e3439f5cbe706ad7f5d420bd0
rogerok <fedorik833@gmail.com>
2026-02-10 10:29:33 +0300

    changed return type of parseArray

--- 2.oop-structures/objects/practice/query-string/index.ts
+++ 2.oop-structures/objects/practice/query-string/index.ts
@@ -15,24 +15,24 @@ import {
  *
  * */
 
-const parseArray = (val: unknown[], objKey: string): string => {
-  return val.reduce<string>((acc, v, idx) => {
+const parseArray = (val: unknown[], objKey: string): string[] => {
+  return val.reduce<string[]>((acc, v, idx) => {
     const withIndex = getIndices(objKey, String(idx));
 
     if (isObject(v)) {
-      acc += buildQueryString(v, withIndex);
+      acc.push(buildQueryString(v, withIndex));
       return acc;
     }
 
     if (Array.isArray(v)) {
-      acc += parseArray(v, withIndex);
+      acc.push(...parseArray(v, withIndex));
       return acc;
     }
 
-    acc += isFirstIdx(idx) ? `${objKey}=${String(v)}` : `&${objKey}=${String(v)}`;
+    acc.push(`${objKey}=${String(v)}`);
 
     return acc;
-  }, "");
+  }, []);
 };
 
 export const buildQueryString = (obj: Record<string, unknown>, prefix?: string): string => {
@@ -47,7 +47,9 @@ export const buildQueryString = (obj: Record<string, unknown>, prefix?: string):
     const k = prefix ? getIndices(prefix, key) : key;
 
     if (Array.isArray(v)) {
-      acc += isFirstIdx(idx) ? parseArray(v, k) : addAmpersand(parseArray(v, k));
+      acc += isFirstIdx(idx)
+        ? parseArray(v, k).join("&")
+        : addAmpersand(parseArray(v, k).join("&"));
       return acc;
     }
 
