23186d96ea9527c19c26aafa675412fc8fff14be
vyacheslav <fedorik833@gmail.com>
2026-02-18 13:42:22 +0300

    map cache replaced with lru cache

--- 2.oop-structures/practice/memoize/index.ts
+++ 2.oop-structures/practice/memoize/index.ts
@@ -1,21 +1,25 @@
+import { LRUCache } from "./lru-cache.ts";
+
 type Callback<Args, Return> = (...args: Args[]) => Return;
 
+const DEFAULT_SIZE = 10;
+
 export function memoize<Args, Return>(cb: Callback<Args, Return>): Callback<Args, Return> {
-  const cache: Record<string, Return> = {};
+  const cache: LRUCache<string, Return> = new LRUCache({
+    maxSize: DEFAULT_SIZE,
+  });
 
   const wrapper: Callback<Args, Return> = (...args: Args[]): Return => {
     const argKeys = String(args);
-
-    if (argKeys in cache) {
-      return cache[argKeys];
+    const value = cache.get(argKeys);
+    if (value) {
+      return value;
     } else {
       const callResult = cb(...args);
-      cache[argKeys] = callResult;
+      cache.set(argKeys, callResult);
       return callResult;
     }
   };
-
-  performance.now();
 
   return wrapper;
 }
