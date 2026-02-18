885faacccd3d867a951abf30e4107bb26d4a1c18
vyacheslav <fedorik833@gmail.com>
2026-02-18 13:25:10 +0300

    * memoize func * lru cache

---
+++ 2.oop-structures/practice/memoize/index.ts
@@ -0,0 +1,21 @@
+type Callback<Args, Return> = (...args: Args[]) => Return;
+
+export function memoize<Args, Return>(cb: Callback<Args, Return>): Callback<Args, Return> {
+  const cache: Record<string, Return> = {};
+
+  const wrapper: Callback<Args, Return> = (...args: Args[]): Return => {
+    const argKeys = String(args);
+
+    if (argKeys in cache) {
+      return cache[argKeys];
+    } else {
+      const callResult = cb(...args);
+      cache[argKeys] = callResult;
+      return callResult;
+    }
+  };
+
+  performance.now();
+
+  return wrapper;
+}
---
+++ 2.oop-structures/practice/memoize/lru-cache.ts
@@ -0,0 +1,62 @@
+export interface LruCacheOptions<K, V> {
+  maxSize: number;
+}
+
+interface CacheValue<V> {
+  value: V;
+}
+
+export class LRUCache<K, V> {
+  private cache = new Map<K, CacheValue<V>>();
+  private readonly maxSize: number;
+
+  constructor(options: LruCacheOptions<K, V>) {
+    if (options.maxSize <= 0) {
+      throw new Error("maxSize must be greater than 0");
+    }
+
+    this.maxSize = options.maxSize;
+  }
+
+  clearCache(): void {
+    this.cache.clear();
+  }
+
+  get(key: K): V | undefined {
+    const item = this.cache.get(key);
+
+    if (!item) return undefined;
+
+    this.reorder(key);
+
+    return item.value;
+  }
+
+  delete(key: K): boolean {
+    return this.cache.delete(key);
+  }
+
+  set(key: K, value: V): void {
+    if (this.cache.has(key)) {
+      this.delete(key);
+    } else if (this.cache.size >= this.maxSize) {
+      const oldestKey = this.cache.keys().next().value;
+
+      if (oldestKey !== undefined) {
+        this.delete(oldestKey);
+      }
+    }
+
+    this.cache.set(key, {
+      value: value,
+    });
+  }
+
+  private reorder(key: K): void {
+    const item = this.cache.get(key);
+    if (!item) return;
+
+    this.delete(key);
+    this.cache.set(key, item);
+  }
+}
