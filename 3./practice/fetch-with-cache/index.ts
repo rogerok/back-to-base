ec780b20ac0faf89de2fbff92f335acc4f928ca9
rogerok <fedorik833@gmail.com>
2026-03-10 21:26:49 +0300

    refactored fetchWithCache; deleted tests

--- 3./practice/fetch-with-cache/index.ts
+++ 3./practice/fetch-with-cache/index.ts
@@ -1,37 +1,45 @@
+import { BASE_DELAY, MAX_DELAY } from "./constants.ts";
 import { LRUCache } from "./lru-cache.ts";
+import { ResponseError, ResponseSuccess, ResponseType } from "./types.ts";
 
-const TIMEOUT = 3000;
-const BASE_DELAY = 200;
-
-export function exponentialBackoffWithJitter(
+const exponentialBackoffWithJitter = (
   attempt: number,
   baseDelay = BASE_DELAY,
-  maxDelay = TIMEOUT,
-): number {
+  maxDelay = MAX_DELAY,
+): number => {
   const exponentialDelay = baseDelay * 2 ** attempt;
 
   const cappedDelay = Math.min(exponentialDelay, maxDelay);
 
   return Math.random() * cappedDelay;
-}
+};
 
 const fetchFn = async <T>(url: string, signal: AbortSignal): Promise<ResponseType<T>> => {
-  const resp = await fetch(url, {
-    signal: signal,
-  });
+  try {
+    const resp = await fetch(url, {
+      signal: signal,
+    });
 
-  if (resp.ok) {
-    const json = await resp.json();
+    if (resp.ok) {
+      const json = await resp.json();
+      return {
+        code: resp.status,
+        data: json as T,
+        status: "success",
+        url: url,
+      };
+    } else {
+      return {
+        code: resp.status,
+        error: resp.statusText,
+        status: "error",
+        url: url,
+      };
+    }
+  } catch (e) {
     return {
-      code: resp.status,
-      data: json as T,
-      status: "success",
-      url: url,
-    };
-  } else {
-    return {
-      code: resp.status,
-      error: resp.statusText,
+      code: 0,
+      error: e,
       status: "error",
       url: url,
     };
@@ -46,7 +54,7 @@ const sleep = (ms: number, signal: AbortSignal) => {
       "abort",
       () => {
         clearTimeout(id);
-        reject(signal.reason);
+        reject(new Error(String(signal.reason)));
       },
       {
         once: true,
@@ -58,39 +66,18 @@ const sleep = (ms: number, signal: AbortSignal) => {
 const shouldRetry = (code: number): boolean => code >= 500;
 const shouldNotRetry = (code: number): boolean => code >= 400 && code < 500;
 
-type Status = "error" | "success";
-
-interface ResponseBase {
-  code: number;
-  status: Status;
-  url: string;
-}
-
-interface ResponseSuccess<T = unknown> extends ResponseBase {
-  data: T;
-  status: "success";
-  url: string;
-}
-
-interface ResponseError extends ResponseBase {
-  error: any;
-  status: "error";
-  url: string;
-}
-
-type ResponseType<T> = ResponseError | ResponseSuccess<T>;
-
-// TODO: maybe store also fail request and make retry for them?
 const createCache = <T>(maxSize: number = 10) =>
   new LRUCache<string, ResponseSuccess<T>>({
     maxSize: maxSize,
   });
 
-const cache = createCache<{ id: string }>();
+const cache = createCache<ResponseSuccess<{ id: string }>>();
 
 export const fetchWithCache = async <T>(
   urls: string[],
-  timeout = 10_000,
+  cache: LRUCache<string, ResponseSuccess<T>>,
+  timeout = MAX_DELAY,
+  maxRetry = 5,
 ): Promise<(ResponseError | ResponseSuccess<T>)[]> => {
   const abortController = new AbortController();
   const urlsToHandle: { idx: number; url: string }[] = [];
@@ -99,18 +86,13 @@ export const fetchWithCache = async <T>(
   urls.forEach((url, idx) => {
     const item = cache.get(url);
     if (item) {
-      response.push(item);
-      return;
+      response[idx] = item;
+    } else {
+      urlsToHandle.push({
+        idx: idx,
+        url: url,
+      });
     }
-
-    urlsToHandle.push({
-      idx: idx,
-      url: url,
-    });
-  });
-
-  abortController.signal.addEventListener("abort", () => {
-    console.log("aborted");
   });
 
   const fetchSingle = async (url: string, idx: number) => {
@@ -126,7 +108,7 @@ export const fetchWithCache = async <T>(
 
       cache.set(url, {
         code: resp.code,
-        data: resp.data as unknown as T,
+        data: resp.data,
         status: resp.status,
         url: url,
       });
@@ -149,31 +131,32 @@ export const fetchWithCache = async <T>(
     if (shouldRetry(resp.code)) {
       let attempt = 1;
 
-      let totalTime = 0;
-
       while (true) {
-        const retryDelay = exponentialBackoffWithJitter(attempt);
-
-        if (totalTime + retryDelay >= timeout) {
-          abortController.abort();
-          break;
+        if (attempt > maxRetry) {
+          processFailure({
+            code: 500,
+            error: "Max retry reached",
+            status: "error",
+            url: url,
+          });
         }
 
-        console.time("retrying");
+        const retryDelay = exponentialBackoffWithJitter(attempt);
+
         try {
           await sleep(retryDelay, abortController.signal);
         } catch (e) {
-          response[idx] = {
+          processFailure({
             code: 500,
             error: "TIMEOUT",
             status: "error",
             url: url,
-          };
+          });
+
           throw e;
         }
 
         resp = await fetchFn<T>(url, abortController.signal);
-        console.timeEnd("retrying");
 
         if (resp.status === "success") {
           processSuccess(resp);
@@ -187,11 +170,9 @@ export const fetchWithCache = async <T>(
         }
 
         attempt += 1;
-        totalTime += retryDelay;
       }
     }
 
-    // Abort all requests
     if (shouldNotRetry(resp.code)) {
       abortController.abort();
 
@@ -209,13 +190,29 @@ export const fetchWithCache = async <T>(
     urlsToHandle.map(async (item) => {
       await fetchSingle(item.url, item.idx);
     }),
-  );
+  ).finally(() => {
+    clearTimeout(timeoutId);
+  });
 
   return response;
 };
 
-const resp = await fetchWithCache([
-  "https://dummyjson.com/products/1",
-  "https://dummyjson.com/products/2",
-  "https://dummyjson.com/http/500",
-]);
+const resp = await fetchWithCache(
+  [
+    "https://dummyjson.com/products/1",
+    "https://dummyjson.com/products/2",
+    "https://dummyjson.com/products/3",
+    "https://dummyjson.com/http/500",
+  ],
+  cache,
+);
+
+const resp2 = await fetchWithCache(
+  [
+    "https://dummyjson.com/products/2",
+
+    "https://dummyjson.com/products/3",
+    "https://dummyjson.com/products/4",
+  ],
+  cache,
+);
